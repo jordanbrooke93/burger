ad4eee308d912e864ed1cfea3a39ebe0eee8f0ab
jordanbrooke93 <jordantaylor93@me.com>
2018-10-24 13:04:46 -0400

    jaws added

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require ("require");
 var exhandlebars = require ("express-handlebars");
 var mysql = require ("mysql");
-
+ if (process.env.JAWSDB_URL) {
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
+ } else {
+   connection = mysql.createConnection({
+     host: "localhost",
+     user: "root",
+     password: "root",
+     database: "burgers_db"
+   })
+ }
 var PORT = process.env.PORT || 3000;
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
